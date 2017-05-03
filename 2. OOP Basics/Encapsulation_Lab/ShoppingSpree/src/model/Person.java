6301f78eac905218c3ab384f5fd8caa38033427e
komitoff <tobo_91@abv.bg>
2017-05-03 18:33:57 +0300

    Implemented person base structure

--- 2. OOP Basics/Encapsulation_Lab/ShoppingSpree/src/model/Person.java
+++ 2. OOP Basics/Encapsulation_Lab/ShoppingSpree/src/model/Person.java
@@ -1,7 +1,39 @@
 package model;
 
-/**
- * Created by tobieski on 5/3/2017.
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class Person {
+
+    private String name;
+    private double cash;
+    private List<Product> bagOfProducts;
+
+    public Person (String name, double cash) {
+        this.setName(name);
+        this.setCash(cash);
+        this.bagOfProducts = new ArrayList<Product>();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    private void setName(String name) {
+        if (name.length() < 1) {
+            throw new IllegalArgumentException("Name must be at least 1 symbol long");
+        }
+        this.name = name;
+    }
+
+    public double getCash() {
+        return this.cash;
+    }
+
+    private void setCash(double cash) {
+        if (cash < 0 ) {
+            throw new IllegalArgumentException("Cash cannot be negative");
+        }
+        this.cash = cash;
+    }
 }
