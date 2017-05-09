698482da1bf1f6be59465612f84546f8f7bd549c
komitoff <tobo_91@abv.bg>
2017-05-09 17:51:10 +0300

    Successfully consumed rest service from https://gturnquist-quoters.cfapps.io/api/random

--- 3. Spring.io Guides/ConsumingRestService/src/main/java/main/App.java
+++ 3. Spring.io Guides/ConsumingRestService/src/main/java/main/App.java
@@ -1,15 +1,35 @@
 package main;
 
 import model.Quote;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.logging.Logger;
 
 @SpringBootApplication
 public class App {
+    private static final org.slf4j.Logger log = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args) {
-        RestTemplate restTemplate = new RestTemplate();
-        Quote quote = restTemplate.getForObject(
-                "https://gturnquist-quoters.cfapps.io/api/random", Quote.class
-        );
+        SpringApplication.run(App.class);
+    }
+
+    @Bean
+    public RestTemplate restTemplate (RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+    @Bean
+    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+        return args -> {
+                Quote quote = restTemplate.getForObject(
+                "https://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+                log.info(quote.toString());
+        };
     }
 }
