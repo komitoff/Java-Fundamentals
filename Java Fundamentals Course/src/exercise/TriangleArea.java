bf9ef15745def2ac984a1e278457261a2cd945f9
komitoff <tobo_91@abv.bg>
2017-09-20 18:38:07 +0300

    Started the exercise homework.

---
+++ Java Fundamentals Course/src/exercise/TriangleArea.java
@@ -0,0 +1,10 @@
+package exercise;
+
+import java.util.Scanner;
+
+public class TriangleArea {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double 
+    }
+}
