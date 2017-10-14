98c4ce0948b6e3b2aaab0993cfaa2600b1908f17
komitoff <tobo_91@abv.bg>
2017-10-14 16:29:08 +0300

    update: task 3 100/100 (NMS.java)

--- Java Fundamentals Course/Exam22-08/src/sometests/Testing.java
+++ Java Fundamentals Course/Exam22-08/src/sometests/Testing.java
@@ -7,11 +7,6 @@ import java.util.Stack;
 
 public class Testing {
     public static void main(String[] args) {
-        Queue<Integer> stack = new LinkedList<>();
-        stack.add(1);
-        stack.add(2);
-        System.out.println(stack.poll());
-        System.out.println(stack.poll());
-        System.out.println(stack.poll());
+        System.out.println((int)'z');
     }
 }
